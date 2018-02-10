d36dab5c1bc4d1e11d93d6a0802b1af4f4eb3cb1
Roman Kasparian <roman.kasparian@gmail.com>
2018-02-10 14:27:42 -0500

    testing database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,30 +25,47 @@ export class AppComponent {
   //     console.log(response);
   //   });
   // }
+  objectkeys(object) {
+    return Object.keys(object);
+  }
   addSource(source) {
     this.sources[source] = {};
     //this.sources.length = this.sources.length + 1;
     console.log(this);
     console.log(this.sources);
-    console.log(this.sources.length);
+    console.log(Object.keys(this.sources).length);
   }
   removeSource(source) {
     delete this.sources[source];
   }
   updateResults(source, results) {
-    this.sources[source].results = results;
+    console.log(results);
+    var avg = 0;
+    var numresults = results.length;
+    for (var result in results) {
+      //this.sources[source].results.push({ score:results[result].documentSentiment.score, magnitude:results[result].documentSentiment.magnitude});
+      avg += results[result].documentSentiment.score;
+    }
+    avg /= numresults;
+    this.sources[source].results = avg;
   }
   analyze(source) {
-    this.bialysis.bialysis(this.search, source).then((response) => {
-      console.log(response);
-      this.updateResults(source, response);
-    }).catch((err) => { console.log(err) });
-     this.bialysis.test().then((response) => {
-       console.log(response);
-     });
+    // if (this.bialysis.hasarticles(this.search, source)) {
+    //   console.log("testing local db");
+    //   this.bialysis.test(this.search, source).then((response) => {
+    //     console.log(response);
+    //     this.updateResults(source, response);
+    //   });
+    // } else {
+      console.log("scraping google");
+      this.bialysis.bialysis(this.search, source).then((response) => {
+        console.log(response);
+        this.updateResults(source, response);
+      }).catch((err) => { console.log(err) });
+    // }
   }
   analyzeAll() {
-    for(var source in this.sources) {
+    for (var source in this.sources) {
       this.analyze(source);
     }
   }
