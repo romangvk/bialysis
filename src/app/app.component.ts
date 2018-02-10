f40f1a8603a1cc133fb26bb4e92eeee77360192d
Roman Kasparian <roman.kasparian@gmail.com>
2018-02-10 08:45:40 -0500

    added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,46 @@ import { BialysisService } from './bialysis.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title;
+export class AppComponent {
+  search;
+  sources;
+  newsource;
   constructor(private bialysis: BialysisService) {
-    this.title = 'waiting';
+    this.search = null;
+    this.sources = {};
+    this.newsource = "";
   }
-  ngOnInit() {
-    this.bialysis.bialysis('poptart', 'washingtonpost.com').then((response) => {
+  // ngOnInit() {
+  //   this.bialysis.bialysis('poptart', 'washingtonpost.com').then((response) => {
+  //     console.log(response);
+  //     this.title = response;
+  //   }).catch((err) => { console.log(err) });
+  //   this.bialysis.test().then((response) => {
+  //     console.log(response);
+  //   });
+  // }
+  addSource(source) {
+    this.sources[source] = {};
+    console.log(this.sources);
+  }
+  removeSource(source) {
+    delete this.sources[source];
+  }
+  updateResults(source, results) {
+    this.sources[source].results = results;
+  }
+  analyze(source) {
+    this.bialysis.bialysis(this.search, source).then((response) => {
       console.log(response);
-      this.title = response;
+      this.updateResults(source, response);
     }).catch((err) => { console.log(err) });
     // this.bialysis.test().then((response) => {
     //   console.log(response);
     // });
+  }
+  analyzeAll() {
+    for(var source in this.sources) {
+      this.analyze(source);
+    }
   }
 }
